b1de34172c2301bba1614d8ed3bb6ebdef50a972
wawaHyun <sjrkchdkdy@gmail.com>
2024-05-24 19:54:34 +0900

    240524

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -11,28 +11,18 @@ const Home:NextPage=()=> {
 
   return (
     <div>
-      <div className="w-screen h-screen justify-center content-center bg-gray-950">
+      <div className="w-screen h-screen justify-center content-center">
         <div className="text-center content-center bg-center bg-bg_img bg-cover h-[70%]">
 
-          <span className="text-gray-300 text-6xl font-bold font-['Inter'] ">TEST TEST TEST TEST TEST!<br /></span>
-          <span className="text-gray-400 text-3xl font-['Inter']">test page입니다!</span><br />
+          <span className="text-6xl font-bold font-['Inter'] ">TEST TEST TEST TEST TEST!<br /></span>
+          <span className="text-3xl font-['Inter']">test page입니다!</span><br />
 
           <div className="mt-[15%]">
             <button className="w-44 h-16 bg-gray-950 rounded-lg hover:bg-pink-700" onClick={()=>router.push(PG.USER+"/login")}>
               <div className="text-white font-medium font-['Inter'] leading-normal">s t a r t </div>
             </button>
 
-            <button className="w-44 h-16 bg-gray-950 rounded-lg hover:bg-pink-700" onClick={()=>router.push(PG.SE+"/news")}>
-              <div className="text-white font-medium font-['Inter'] leading-normal">SE news! </div>
-            </button>
-            <button className="w-44 h-16 bg-gray-950 rounded-lg hover:bg-pink-700" 
-            onClick={()=>window.open("C:\\Users\\bitcamp\\Gisa\\lixgit\\front\\src\\app\\pages\\jusik\\SE\\news\\kr-state.html")}>
-              <div className="text-white font-medium font-['Inter'] leading-normal">
-                {/* <Component onClick={()=>window.open("C:\\Users\\bitcamp\\Gisa\\lixgit\\front\\src\\app\\pages\\jusik\\SE\\news\\kr-state.html")}></Component> */}
-                SE chart 
-
-                </div>
-            </button>
+            
 
           </div>
 
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "bg_img": "url('https://media.istockphoto.com/id/1262998467/ko/%EC%82%AC%EC%A7%84/%EB%AF%B8%EA%B5%AD-%EB%8B%AC%EB%9F%AC-%EC%9C%A0%EB%A1%9C-%ED%94%84%EB%9E%AD%ED%81%AC-%EC%97%94-%EA%B8%88%EC%9C%B5-%EB%8F%88-%EA%B8%80%EB%A1%9C%EB%B2%8C-%EA%B8%88%EC%9C%B5-%EC%A3%BC%EC%8B%9D-%EC%8B%9C%EC%9E%A5-%EB%B0%B0%EA%B2%BD.jpg?s=612x612&w=0&k=20&c=oSd8A2LVGFOjVcomW6kRI9EQHvp11-JHXfXOzxSikoQ=')",
+          "bg_img": "url('https://img.freepik.com/premium-photo/abstract-forex-chart-with-candlestick-graph-index-tech-hologram-blurry-bright-city-background-trade-stock-finance-concept-double-exposure_670147-11705.jpg?w=996')",
 
       },
     },
